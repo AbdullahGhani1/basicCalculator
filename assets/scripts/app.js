a03752c47d877748bbaa100a09166e0ca7cfcc38
engr_ghani <engr.abdullahghani@gmail.com>
2020-06-28 13:09:41 +0500

    Update app.js
    Adding log functionality to all basic function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,31 +12,48 @@ function createAndWriteOutput(operator, beforeCalc, calcNumber) {
   //   function calling from vender.js
   outputResult(currentResult, calcDescription);
 }
+function logEntry(
+  operationIdentifier,
+  initialResult,
+  enteredNumber,
+  currentResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
 function add() {
   const enteredNumber = getUserumberInput();
   const initialResult = currentResult;
   currentResult = currentResult + enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
-  logEntries.push(enteredNumber);
-  console.log(logEntries);
+  logEntry("Add", initialResult, enteredNumber, currentResult);
 }
 function subtruct() {
   const enteredNumber = getUserumberInput();
   const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
   createAndWriteOutput("-", initialResult, enteredNumber);
+  logEntry("Subtruct", initialResult, enteredNumber, currentResult);
 }
 function multiply() {
   const enteredNumber = getUserumberInput();
   const initialResult = currentResult;
   currentResult = currentResult * enteredNumber;
   createAndWriteOutput("*", initialResult, enteredNumber);
+  logEntry("Multiply", initialResult, enteredNumber, currentResult);
 }
 function divide() {
   const enteredNumber = getUserumberInput();
   const initialResult = currentResult;
   currentResult = currentResult / enteredNumber;
   createAndWriteOutput("/", initialResult, enteredNumber);
+  logEntry("Division", initialResult, enteredNumber, currentResult);
 }
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtruct);
